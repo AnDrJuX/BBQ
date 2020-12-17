8c00b6673284db648f5d4be33eba62bbba452d36
andres peterson <andres.von.peterson@gmail.com>
2020-12-17 16:18:42 +0200

    add forms & flash

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -2,4 +2,12 @@ module ApplicationHelper
   def user_avatar(user)
     asset_pack_path('media/images/08.png')
   end
+
+  def flash_class_name(name)
+    case name
+    when 'notice' then 'success'
+    when 'alert'  then 'danger'
+    else name
+    end
+  end
 end
