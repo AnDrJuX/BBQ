6cb00c1a341bb3e13ff7cacdeccbb83a72511cc5
andres peterson <andres.von.peterson@gmail.com>
2020-12-20 08:58:33 +0200

    delete json

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -4,13 +4,11 @@ class EventsController < ApplicationController
   before_action :set_current_user_event, only: [:edit, :update, :destroy]
 
   # GET /events
-  # GET /events.json
   def index
     @events = Event.all
   end
 
   # GET /events/1
-  # GET /events/1.json
   def show
   end
 
@@ -24,7 +22,6 @@ class EventsController < ApplicationController
   end
 
   # POST /events
-  # POST /events.json
   def create
     @event = current_user.events.build(event_params)
 
@@ -36,26 +33,21 @@ class EventsController < ApplicationController
   end
 
   # PATCH/PUT /events/1
-  # PATCH/PUT /events/1.json
   def update
     respond_to do |format|
       if @event.update(event_params)
         format.html { redirect_to @event, notice: I18n.t('controllers.events.updated') }
-        format.json { render :show, status: :ok, location: @event }
       else
         format.html { render :edit }
-        format.json { render json: @event.errors, status: :unprocessable_entity }
       end
     end
   end
 
   # DELETE /events/1
-  # DELETE /events/1.json
   def destroy
     @event.destroy
     respond_to do |format|
       format.html { redirect_to events_url, notice: I18n.t('controllers.events.destroyed.') }
-      format.json { head :no_content }
     end
   end
 
--- app/models/user.rb
+++ app/models/user.rb
@@ -4,6 +4,7 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
   has_many :events
+  has_many :comments, dependent: :destroy
 
   before_validation :set_name, on: :create
 
